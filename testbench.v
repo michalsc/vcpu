2b2e1906362b3852cc998b6f91b440ba239c14e8
Michal Schulz <michal.schulz@gmx.de>
2021-01-18 19:03:07 +0100

    testbench uses ROM for tests

--- testbench.v
+++ testbench.v
@@ -12,43 +12,70 @@
 `timescale 1ns/1ps
 
 module testbench;
- 
+    integer i;
+    reg [31:0] rom[0:1023];
+
     reg reset = 'bZ;
     reg clk = 1;
     reg [1:0] dsack = 2'b11;
-    wire [31:0] D;
+    reg [31:0] D = 32'bZ;
+    reg [31:0] Data;
+    wire as;
+    wire ds;
+    wire [31:0] A;
+    wire RnW;
+    reg br = 'b1;
+    reg bgack = 'b1;
+
+    wire dben;
+
+    always @(negedge dben) begin
+        if (RnW == 'b1) begin
+            #40 D[31:0] <= rom[A[31:2]];
+            #1 dsack <= 'b00;
+        end
+    end
+
+    always @(posedge as or posedge ds) begin
+        dsack <= 'b11;
+        D <= 32'bZ;
+    end
 
     BUS_68020 V020(
         .nRESET(reset),
         .CLK(clk),
         .nDSACK(dsack),
-        .D(D)
+        .D(D),
+        .A(A),
+        .nBR(br),
+        .nBGACK(bgack),
+        .nDBEN(dben),
+        .RnW(RnW),
+        .nAS(as),
+        .nDS(ds)
     );
 
     initial begin
+        $readmemh("rom", rom);
         $dumpfile("testbench.vcd");
         $dumpvars(0, testbench);
+        
+
+        br = 1;
+        bgack = 1;
 
         #5 reset = 0;
         #40 reset = 1;
-        #20 V020.r_BReq = 2;
-        V020.r_AddrReq = 32'h0123451;
-        V020.r_Data = 32'hDEADBEEF;
-        V020.r_SizeReq = 0;
-        #2 V020.r_BReq = 0;
-
-        #10 dsack = 'b01;
-        #2 dsack = 'b11;
-
-        #10 dsack = 'b01;
-        #2 dsack = 'b11;
-
-        #10 dsack = 'b01;
-        #2 dsack = 'b11;
-
-        #10 dsack = 'b01;
-        #2 dsack = 'b11;
-
+        #20 
+        for (i=0; i < 8; i++) begin
+            V020.r_BReq = 1;
+            V020.r_AddrReq = i;
+            V020.r_SizeReq = 0;
+            #2 V020.r_BReq = 0;
+            @(posedge V020.r_BReqComplete);
+            Data <= V020.r_Data;
+            #5;
+        end
 
         #500 $finish;
     end
