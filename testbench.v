0961b21a5656c25f0ae1b1f4e2a253d00c73c1f4
Michal Schulz <michal.schulz@gmx.de>
2021-01-26 21:53:54 +0100

    testing instruction cache

--- testbench.v
+++ testbench.v
@@ -20,13 +20,14 @@ module testbench;
     reg [1:0] dsack = 2'b11;
     reg [31:0] D = 32'bZ;
     reg [31:0] Data;
+    reg [31:0] Ain;
     wire as;
     wire ds;
     wire [31:0] A;
     wire RnW;
     reg br = 'b1;
     reg bgack = 'b1;
-
+    reg [2:0] brin;
     wire dben;
 
     always @(negedge dben) begin
@@ -55,11 +56,13 @@ module testbench;
         .nDS(ds)
     );
 
+    assign V020.ICache.BRin = brin;
+    assign V020.ICache.Ain = Ain;
+
     initial begin
-        $readmemh("rom-32bit", rom);
+        $readmemh("rom-aros68k.txt", rom);
         $dumpfile("testbench.vcd");
         $dumpvars(0, testbench);
-        
 
         br = 1;
         bgack = 1;
@@ -67,6 +70,33 @@ module testbench;
         #5 reset = 0;
         #40 reset = 1;
         #20 
+
+        for (i=0; i < 16; i++) begin
+            Ain=i*2;
+            brin = 1;
+            @(posedge V020.ICache.completed);
+            brin = 0;
+            #1;
+        end
+/*
+        for (i=0; i < 16; i++) begin
+            Ain=i*2;
+            brin = 1;
+            @(posedge V020.ICache.completed);
+            brin = 0;
+            #1;
+        end
+
+        for (i=0; i < 16; i++) begin
+            Ain=i*2;
+            brin = 1;
+            @(posedge V020.ICache.completed);
+            brin = 0;
+            #1;
+        end
+*/
+        
+/*
         for (i=0; i < 8; i++) begin
             V020.r_BReq = 1;
             V020.r_AddrReq = i;
@@ -76,7 +106,7 @@ module testbench;
             Data <= V020.r_Data;
             #5;
         end
-
+*/
         #500 $finish;
     end
 
